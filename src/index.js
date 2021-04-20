ae14b02ed63993e5b2e39d31330fed936ffd1119
min <minwang.bus@gmail.com>
2021-04-20 12:03:10 -0400

    working on DOMContent

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
+require 
 console.log("Webpack is working!")
 
 const MovingObject = require("./moving_object.js");
 window.MovingObject = MovingObject;
+module.exports = {
+canvas: document.addEventListener("DOMContentLoaded", () => {
+    const canvasEle = document.getElementById("game-canvas");
+    canvasEle.width = 500;
+    canvasEle.height = 500;
+    const ctx = canvasEle.getContext("2d");
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 500, 500);
+})
+}
--- src/moving_object.js
+++ src/moving_object.js
@@ -5,6 +5,20 @@ function MovingObject(options) {
     this.color = options.color;
 };
 
+MovingObject.prototype.draw = function(ctx) {
+    ctx.fillstyle = this.color;
+    ctx.beginPath();
+    ctx.arc(
+        this.pos[0],
+        this.pos[1],
+        this.radius,
+        0,
+        2 * Math.PI,
+        false
+    );
+    
+    ctx.fill();
+}
 module.exports = MovingObject;
 
 
